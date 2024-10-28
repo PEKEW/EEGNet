1764df49d12705ea1c85aec9ebef406310783865
PEKEW <pekewang@hotmail.com>
2024-10-28 11:13:06 +0800

    svm部分正确完成 类别均衡采样完成 包括rmse在内的一系列metric计算

--- models/SVM.py
+++ models/SVM.py
@@ -2,6 +2,7 @@ import numpy as np
 import ast
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import classification_report, confusion_matrix
 import json
@@ -26,18 +27,36 @@ class SVMClassifier:
                 slice_features = []
                 for frame in frames.values():
                     pos = ast.literal_eval(frame['pos'])
-                    slice_features.extend(
+                    slice_features.append([               
                         float(pos[0]), float(pos[1]), float(pos[2]),
-                        float(frame['time']),
+                        float(frame['time_']),
                         float(frame['speed']),
                         float(frame['acceleration']),
-                        float(frame['rotationSpeed'])
-                    )
-                avg_features = np.mean(np.array(slice_features).reshape(-1, ), axis=0)
+                        float(frame['rotation_speed'])
+                    ])
+                slice_features = np.array(slice_features)
+                avg_features = np.mean(slice_features, axis=0)
                 x.append(avg_features)
                 y.append(labels[subj][slice_id])
         self.data = np.array(x)
         self.labels = np.array(y)
+        num_samples = len(self.data)
+        print(f"Number of samples: {num_samples}")
+        # 找到最小的样本数量
+        unique_labels, counts = np.unique(self.labels, return_counts=True)
+        min_count = min(counts)
+        balanced_data = []
+        balanced_labels = []
+        # 对于每个标签 随机选择min_count个索引
+        for label in unique_labels:
+            indices = np.where(self.labels == label)[0]
+            selected_indices = random.sample(list(indices), min_count)
+            balanced_data.extend(self.data[selected_indices])
+            balanced_labels.extend(self.labels[selected_indices])
+        self.data = np.array(balanced_data)
+        self.labels = np.array(balanced_labels)
+        print(f"Balanced number of samples: {len(self.data)}")
+
     def train_fit(self, test_size=0.2, random_state=42):
         X_train, X_test, y_train, y_test = train_test_split(self.data, self.labels, test_size=test_size, random_state=random_state)
         scaler = StandardScaler()
@@ -52,6 +71,8 @@ class SVMClassifier:
         print(confusion_matrix(y_test, y_pred))
         print("\nClassification Report:")
         print(classification_report(y_test, y_pred))
+        mse = mean_squared_error(y_test, y_pred)
+        print(f"Mean Squared Error: {mse}")
 
 
 
